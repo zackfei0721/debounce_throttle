0d48b6a740511b1ba9cd333067cd33e2d8b45752
zackfei0721 <zackfei0721@gmail.com>
2023-06-21 20:58:12 -0400

    fixed leading & trailing for debounce

--- src/debounce.js
+++ src/debounce.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
-function debounce(fn, delay) {
+function debounce(fn, delay, options = {}) {
     // debounce: wait and invoke later
     let timer;
+    let firstInvoke = true;
+    let {leading = false, trailing = true} = options;
     return function(...args) {
+        const context = this;
+        if (firstInvoke && leading) {
+            fn.apply(context, args);
+            firstInvoke = false;
+        }
         clearTimeout(timer);
-        timer = setTimeout(() => fn.apply(this, args), delay);    
+        if (trailing) {
+            timer = setTimeout(() => {
+                fn.apply(context, args); 
+                firstInvoke = true;
+             }, delay);  
+            } else {
+                timer = setTimeout(() => {
+                    firstInvoke = true;
+                }, delay);
+            }
+        }
     }
-}
 
 function DebounceBtn() {
+    const options = { leading: true, trailing: true };
     const debouncedOnClick = React.useCallback(debounce(() => {
         console.log('debounced btn clicked!');
-    }, 2000), {});
+        console.log('leading:', options.leading, ' trailing:', options.trailing);
+    }, 2000, options), []);
 
     return (<button onClick={debouncedOnClick}>Debounced Button</button>);
 }
